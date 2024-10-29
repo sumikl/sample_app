85e6dac0b93915a14bb59ba6d89fd86680bbe130
sumikl <111672097+sumikl@users.noreply.github.com>
2024-10-29 13:09:47 +0000

    add contolorer

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
-class StaticPagesController < ApplicationController
-  def home
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test 'should get home' do
+    get static_pages_home_url
+    assert_response :success
   end
 
-  def help
+  test 'should get help' do
+    get static_pages_help_url
+    assert_response :success
   end
 end
