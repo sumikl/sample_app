88e34b5309e4445a5452cb65bf00ccaab7a564f3
sumikl <111672097+sumikl@users.noreply.github.com>
2024-10-29 14:06:16 +0000

    viwe refactering

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,7 @@
-require 'test_helper'
+class StaticPagesController < ApplicationController
+  def home; end
 
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get static_pages_home_url
-    assert_response :success
-  end
+  def help; end
 
-  test 'should get help' do
-    get static_pages_help_url
-    assert_response :success
-  end
+  def about; end
 end
